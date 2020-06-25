f368ac686ba1ad08da2f7e3d51ff353f55a3cbce
Rajib Dey <raj.kuet@gmail.com>
2020-06-26 00:51:56 +0600

    Update scce-created-shortcodes.php

--- admin/scce-created-shortcodes.php
+++ admin/scce-created-shortcodes.php
@@ -79,3 +79,23 @@ if ( ! function_exists( "scce_shortcode_test_code_6" ) ) {
 		<?php return ob_get_clean();
 	}
 }
+add_shortcode( "google-map", "scce_shortcode_google_map" );
+if ( ! function_exists( "scce_shortcode_google_map" ) ) {
+	function scce_shortcode_google_map( $params, $content = null ) {
+		$scce_sc_atts = shortcode_atts( array(
+		), $params );
+		ob_start(); ?>
+		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3168.639290621064!2d-122.08624618513969!3d37.42199987982517!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fba02425dad8f%3A0x6c296c66619367e0!2sGoogleplex!5e0!3m2!1sen!2sbd!4v1593098084691!5m2!1sen!2sbd" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+		<?php return ob_get_clean();
+	}
+}
+add_shortcode( "youtube-video", "scce_shortcode_youtube_video" );
+if ( ! function_exists( "scce_shortcode_youtube_video" ) ) {
+	function scce_shortcode_youtube_video( $params, $content = null ) {
+		$scce_sc_atts = shortcode_atts( array(
+		), $params );
+		ob_start(); ?>
+		<iframe width="560" height="315" src="https://www.youtube.com/embed/U6nCBM1cgxc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		<?php return ob_get_clean();
+	}
+}
