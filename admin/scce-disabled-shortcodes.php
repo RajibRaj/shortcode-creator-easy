5f0e94949b8f2d1cfbddba59189d41dabd86d5e9
Rajib Dey <raj.kuet@gmail.com>
2020-06-26 05:10:01 +0600

    Create scce-disabled-shortcodes.php

---
+++ admin/scce-disabled-shortcodes.php
@@ -0,0 +1,12 @@
+<?php
+// if this file is called directly, abort
+if ( ! defined( 'ABSPATH' ) ) die( 'Direct access denied!' );
+
+/**
+ * The file stores all the information for the shortcodes
+ * that created and disabled using this plugin
+ *
+ * @since      1.0.0
+ * @package    Shortcode_Creator_Easy
+ * @subpackage Shortcode_Creator_Easy/includes
+ */
